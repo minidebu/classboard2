4f8eadd08c4f9f348af7a35b00bceb5acc2a897e
minidebu <debudebo3@gmail.com>
2022-04-30 13:02:24 +0900

    フォームの作成

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,21 @@
 class StudentsController < ApplicationController
   def index
+    @student = Student.new
+    
+  end
+  def create
+    @student = Student.new(student_params)
+    if @student.save 
+      redirect_to root_path
+    else
+      render action: :new
+    end
+
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:kana_name,:name,:birth_on)
+    
   end
 end
