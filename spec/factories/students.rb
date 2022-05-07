52bbce35aca462dc3907bf7d0d38631a9e2b7286
minidebu <debudebo3@gmail.com>
2022-05-07 11:16:01 +0900

    テスト

--- spec/factories/students.rb
+++ spec/factories/students.rb
@@ -1,8 +1,7 @@
 FactoryBot.define do
   factory :student do
-    name                {"田中"}
-    kana_name           {"タナカ"}
-    birth_on            { Faker::Date.backward }
-
+    name                  {"松本"}
+    kana_name             {"マツモト"}
+    birth_on              {Faker::Date.between(from: 5.days.ago, to: Date.today)}
   end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,40 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
+
   before do
     @student = FactoryBot.build(:student)
   end
 
-  describe "生徒の登録"do
-    context "生徒が登録できる時" do
-      it '入力が正しい時、生徒が登録される。'do
+  describe "生徒管理機能" do
+    context "生徒の登録ができる" do
+      it "入力が正常な時、生徒が登録できる" do
         expect(@student).to be_valid
       end
     end
-    context "生徒が登録できない時"  do
-      it "名前の入力がない時生徒を登録できない" do
-        @student.name = ''
+    context "生徒の登録ができない" do
+      it "名前を入力していなければ、生徒の登録ができない" do
+        @student.name = ""
         @student.valid?
         expect(@student.errors.full_messages).to include("Name can't be blank")
-      end
+   
 
-      it "カナの入力がない時生徒を登録できない" do
-        @student.kana_name = ''
+      end
+      it "カナを入力していなければ、生徒の登録ができない" do
+        @student.kana_name = ""
         @student.valid?
         expect(@student.errors.full_messages).to include("Kana name can't be blank")
-      end
 
-      it "カナの入力が全角カナでない時生徒を登録できない" do
-        @student.kana_name = "あいえお"
+
+      end
+      it "誕生日を入力していなければ、生徒の登録ができない" do
+        @student.birth_on = ""
+        @student.valid?
+        expect(@student.errors.full_messages).to include("Birth on can't be blank")
+
+      end
+      it "カナに全角カタカナ以外の文字を入力していれば、生徒の登録ができない" do
+        @student.kana_name = "あいうえお"
         @student.valid?
         expect(@student.errors.full_messages).to include("Kana name 全角カナを入力してください")
       end
+    end
 
-      it '誕生日が空があれば、ユーザー登録ができない' do
-        @student.birth_on = "--/--/--"
-        @student.valid?
-        expect(@student.errors.full_messages).to include("Birth on can't be blank")
+    context "退会登録" do
+      it "退会日を正常に入力すれば、退会登録ができる" do
+        @student.withdrawal_on = Faker::Date.between(from: 5.days.ago, to: Date.today)
+        expect(@student).to be_valid
       end
     end
+
+
   end
 end
