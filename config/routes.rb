93b84ab530acc5399e41d180ed8872cedd959ea8
minidebu <debudebo3@gmail.com>
2022-05-21 13:02:07 +0900

    カレンダーの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'plans#index'
+  root to: 'calendars#index'
   resources :students ,only:[:index,:new,:create,:update,:show]  do
     resources :schedule_time_tables,only:[:index ,:create]
-#   resources :student_schedules ,only:[:index ,:create]   
-    # resources :student_time_tables,only:[:index,:create]
   end
+  resources :calendars ,onle:[:index]
   resources :time_tables,only:[:create]
   resources :schedules,only:[:index,:create]
   resources :plans ,only:[:index,:new,:create]
