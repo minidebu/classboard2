6b657b6ff661a53d23704fedb6b43636e829d20f
minidebu <debudebo3@gmail.com>
2022-06-05 16:28:46 +0900

    クリックした日の出席者表示

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,25 +8,44 @@ class CalendarsController < ApplicationController
 
     if params[:s_day].present?
       make_students_day(params[:s_day])
-      binding.pry
-      partial = render_to_string(partial:'footer', :locals => { month_plans:@month_plans })
+      partial = render_to_string(partial:'footer', :locals => { day_plans:@day_plans })
       render json:{html:partial}    
     end
 
-
-
   end
-
-
-
-
 
   private
   # １日分の生徒リストを出す
   def make_students_day(s_day)
-    num_week = week_from_date(s_day)
-
+      num_week = week_from_date(s_day)
+    @day_plans = [] 
+    
+    case num_week[:num] 
+    when 1 then
+      d_plans =  @month_plans.where(week_id: num_week[:wday]).where.not(one_week_id:1)
+      d_plans.order(st_time:"ASC").each do |d_plan|
+        @day_plans <<{name:d_plan.name,st_time:d_plan.st_time,lesson_id:d_plan.one_week_id}
+      end
+    when 2 then
+      d_plans =  @month_plans.where(week_id: num_week[:wday]).where.not(two_week_id:1)
+      d_plans.order(st_time:"ASC").each do |d_plan|
+        @day_plans <<{name:d_plan.name,st_time:d_plan.st_time,lesson_id:d_plan.two_week_id}
+      end
+    when 3 then
+      d_plans =  @month_plans.where(week_id: num_week[:wday]).where.not(three_week_id:1)
+      d_plans.order(st_time:"ASC").each do |d_plan|
+        @day_plans <<{name:d_plan.name,st_time:d_plan.st_time,lesson_id:d_plan.three_week_id}
+      end
+    when 4 then
+      d_plans =  @month_plans.where(week_id: num_week[:wday]).where.not(four_week_id:1)
+      d_plans.order(st_time:"ASC").each do |d_plan|
+        @day_plans <<{name:d_plan.name,st_time:d_plan.st_time,lesson_id:d_plan.four_week_id}
+     
+      end
+    end
   end
+
+
 
 
 
@@ -40,8 +59,14 @@ class CalendarsController < ApplicationController
      end
     nth = d_day.cweek - beginning_of_month_cweek
     nth = s_day.ago(1.week).to_date.cweek - beginning_of_month_cweek if nth.negative?
-    binding.pry
-    return { num: nth, wday: d_day.wday }
+    
+
+    d_wday = d_day.wday
+    if d_wday == 0
+      d_wday = 7
+    end
+
+    return { num: nth, wday: d_wday }
   end
 
   # シンプルカレンダーを日曜始まりにする
@@ -80,15 +105,15 @@ class CalendarsController < ApplicationController
     end
     @month_plans.where.not(two_week_id:1).group(:st_time,:week_id).count.each do |mpc|  
       name =mpc[0][0].strftime("%k:%M") + " #{mpc[1].to_s}人"
-      Plan.create(name:name,start_time:date_from_week(2,mpc[0][1] ,s_date,mpc[0][0]),count:mpc[1],week_id:mpc[0][1],st_time:mpc[0][0],num_week:1)
+      Plan.create(name:name,start_time:date_from_week(2,mpc[0][1] ,s_date,mpc[0][0]),count:mpc[1],week_id:mpc[0][1],st_time:mpc[0][0],num_week:2)
     end
     @month_plans.where.not(three_week_id:1).group(:st_time,:week_id).count.each do |mpc|  
       name =mpc[0][0].strftime("%k:%M") + " #{mpc[1].to_s}人"
-      Plan.create(name:name,start_time:date_from_week(3,mpc[0][1] ,s_date,mpc[0][0]),count:mpc[1],week_id:mpc[0][1],st_time:mpc[0][0],num_week:1)
+      Plan.create(name:name,start_time:date_from_week(3,mpc[0][1] ,s_date,mpc[0][0]),count:mpc[1],week_id:mpc[0][1],st_time:mpc[0][0],num_week:3)
     end
     @month_plans.where.not(four_week_id:1).group(:st_time,:week_id).count.each do |mpc|  
       name =mpc[0][0].strftime("%k:%M") + " #{mpc[1].to_s}人"
-      Plan.create(name:name,start_time:date_from_week(4,mpc[0][1] ,s_date,mpc[0][0]),count:mpc[1],week_id:mpc[0][1],st_time:mpc[0][0],num_week:1)
+      Plan.create(name:name,start_time:date_from_week(4,mpc[0][1] ,s_date,mpc[0][0]),count:mpc[1],week_id:mpc[0][1],st_time:mpc[0][0],num_week:4)
     end
 
     @plans = Plan.all
@@ -122,7 +147,6 @@ class CalendarsController < ApplicationController
     t.integer :student_id
     t.string  :name
     t.integer :week_id
-    t.string  :week
     t.time    :st_time
     t.integer :one_week_id
     t.integer :two_week_id
@@ -147,7 +171,7 @@ class CalendarsController < ApplicationController
   
         stu_tt = stu_tts.where('started_on <= ? ',s_date.beginning_of_month).last
         stu_sd = stu_sds.where('started_on <= ? ',s_date.beginning_of_month).last
-        MonthPlan.create(student_id:student.id,name:stu_tt.student.name,week_id:stu_tt.time_table.week_id,week:stu_tt.time_table.week.name, 
+        MonthPlan.create(student_id:student.id,name:stu_tt.student.name,week_id:stu_tt.time_table.week_id,
           st_time:stu_tt.time_table.st_time,one_week_id:stu_sd.schedule.one_week_id,two_week_id:stu_sd.schedule.two_week_id,
           three_week_id:stu_sd.schedule.three_week_id,four_week_id:stu_sd.schedule.four_week_id)
       end
